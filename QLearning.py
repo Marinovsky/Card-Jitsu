6a58aa50ad8a71643ad185cf205224c5d2768933
Ricardo Andrés Marino Rojas <rmarino@unal.edu.co>
2022-12-01 13:07:17 -0500

    Add docs and update QTable.json

--- QLearning.py
+++ QLearning.py
@@ -1,3 +1,5 @@
+# Card-Jitsu game is represented as a Markov Decision Process (MDP)
+
 # States are represented as "[AI Elements]$[Player Elements]"
 # where AI Elements is a string containing "F", "A", "N" meaning
 # the elements cards of which the ai has won a round. I.E For
@@ -9,24 +11,74 @@
 # will be "FN$A", because for both, the ai and the player, the elements
 # are stored in alphabetic order (this is made with function ComputeNewState())
 
+# Actions are represented as a string list of possible cards to play
+
+# Rewards are calculated by the function Reward()
+
+# Transition model is implemented in the function TransitionModel()
+
 import json
 import random
 
+# Card-Jitsu cards, actions the AI agent and the player can take
 cards = ["F1", "A1", "N1", "F2", "A2", "N2", "F3", "A3", "N3"]
+
+# Discounting factor for the AI agent
+gamma = 0.4
+
+# Seed to make the experiments reproducible
 random.seed(0)
 
-# Read Q-Table
 def ReadQTable():
+    """
+    Description
+    -----------
+    This funtion loads the Q-table from the .json file, saves it
+    into a dictionary and return this dictionary
+    """
     f = open("QTable.json")
     qTable = json.load(f)
     return qTable
 
 # Save Q-Table
 def SaveQtable(qTable):
+    """
+    Description
+    -----------
+    This function updates the file "QTable.json" with the given
+    dictionary
+
+    Parameters
+    ----------
+    qTable : Dictionary of string-(float list)
+        The qtable obtained from the function QLearning()
+    """
     with open("QTable.json", "w") as outfile:
         json.dump(qTable, outfile)
 
 def ComputeNewState(s, winner, card):
+    """
+    Description
+    -----------
+    Given the last state, the winner of the current round, and
+    the card with which the winner won the round it computer the
+    next state
+
+    Parameters
+    ----------
+    s : string
+        The last state of the game
+    winner : string
+        "ai" if the AI agent won the round and "player" otherwise
+    card : string
+        "A" if the winner won with a water card. "F" if the winner
+        won with a fire card. "N" if the winner won with a snow card.
+
+    Returns
+    -------
+    string : The next state of the game based in the last state, the
+    winner of the last round and the card with wich it won.
+    """
     ai_wins = s.split("$")[0]
     player_wins = s.split("$")[1]
 
@@ -40,6 +92,29 @@ def ComputeNewState(s, winner, card):
         return (ai_wins + "$" + new_player_wins)
 
 def TransitionModel(s, a, a_prime):
+    """
+    Description
+    -----------
+    Function representing the transition model of the Markov Decision
+    Process (MDP) or the game (Card-Jitsu) logic. Based in the last state,
+    the action took by the AI agent and the action took by the player it
+    determined the next state.
+
+    Parameters
+    ----------
+    s : string
+        Last state of the MDP or game
+    a : int
+        Index of the card took by the AI agent, see cards dictionary
+    a_prime : int
+        Index of the card took by the player, see cards dictionary
+
+    Returns
+    -------
+    string : New state of the MDP or game
+    string : winner of the round, "ai" if the AI agent won and "player"
+    otherwise.
+    """
     ia_card = cards[a]
     player_card = cards[a_prime]
 
@@ -83,6 +158,21 @@ def TransitionModel(s, a, a_prime):
             return s, "draw"
 
 def Reward(s):
+    """
+    Description
+    -----------
+    Computes the reward for the AI agent based on the last round.
+
+    Parameters
+    ----------
+    s : string
+        Winner of the last round, "ai" if the AI agent won and
+        "player" otherwise.
+    
+    Returns
+    -------
+    int : 1 if the AI agent won, -1 if the player won and 0 otherwise.
+    """
     if s == "ai":
         return 1
     elif s == "draw":
@@ -91,6 +181,16 @@ def Reward(s):
         return -1
 
 def PrintGameStatus(s_prime):
+    """
+    Description
+    -----------
+    Prints the game status the inform the player.
+
+    Parameters
+    ----------
+    s_prime : string
+        Current game state
+    """
     player = s_prime.split("$")[1]
     ai = s_prime.split("$")[0]
     print("=== MATCH ===")
@@ -99,6 +199,22 @@ def PrintGameStatus(s_prime):
     print("=============")
 
 def CheckIfWinner(s):
+    """
+    Description
+    -----------
+    Function to check whether the AI agent or the player has alredy
+    won the game.
+
+    Parameters
+    ----------
+    s : string
+        Current status of the MDP or game.
+    
+    Returns
+    -------
+    string/int : "ai" if the AI agent won the game, "player" if the player won
+    the game and -1 if neither of them has already won the game
+    """
     ai = s.split("$")[0]
     player = s.split("$")[1]
 
@@ -116,8 +232,24 @@ def CheckIfWinner(s):
     
     return -1
 
-#epsilon-greedy method with epsilon = 0.01
 def SelectAction(list):
+    """
+    Description
+    -----------
+    Function to select the next action of the AI agent using
+    the epsilon-greedy method, with epsilon = 0.01.
+
+    Parameters
+    ----------
+    list : list
+        float list, where the index represent the Card-Jitsu cards
+        and the value of them the Q-Value of them.
+    
+    Returns
+    -------
+    int : The action the AI agent took based on the epsilon-greedy method.
+
+    """
     n = random.randint(1, 100)
     if n != 1:
         return list.index(max(list))
@@ -125,7 +257,21 @@ def SelectAction(list):
         k = random.randint(0,8)
         return k
 
-def QLearning(gamma):
+def Game(gamma):
+    """
+    Description
+    -----------
+    Card-Jitsu game driver to play between a player and the 
+    AI agent, represented by the Q-Table. This can also be 
+    seen as the Markov Decision Process (MDP) of the Card-Jitsu
+    game. This functions allows the user to play with the AI, teaching
+    the AI agent how to play in the process.
+
+    Parameters
+    ----------
+    gamma : float
+        The discounting factor for the Q-Learning AI agent
+    """
     qTable = ReadQTable()
     s = "$"
     alpha = 1
@@ -166,6 +312,6 @@ def QLearning(gamma):
     SaveQtable(qTable)
     print(match)
 
-gamma = 0.4
-QLearning(gamma)
+# Start to play
+Game(gamma)
 
